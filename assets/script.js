2172f6cef021a37c6aa8fa36e2002668e289f60b
Abdelrhman Sayed El-Sadory <abdo_sadory@yahoo.com>
2022-12-24 17:02:24 +0200

    services-section is done

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
 console.log("hi");
+const toTopArrow = document.getElementById("to-top-btn-container");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 400) {
+    toTopArrow.style.display = "block";
+  } else {
+    toTopArrow.style.display = "none";
+  }
+});
